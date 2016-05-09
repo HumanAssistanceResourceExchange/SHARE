389820a1fcd774610e9aa1d8a6757c199d0dcce7
Christine L. Feaster <feasterc@gmail.com>
2016-05-08 18:31:31 -0700

    Add page title for new listings page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,6 +5,7 @@ class ListingsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def new
+    @page_title = "Create New Listing"
     @listing = Listing.new
   end
 
